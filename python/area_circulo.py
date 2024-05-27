27e000c58d4653330c2edd52b259aa281f59c2d1
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2024-05-27 18:40:49 -0300

    commit de area_circulo.py
    Foi criado um script para realizar o cálculo da área de um círculo, correspondente ao problema: Bee 1002

---
+++ python/area_circulo.py
@@ -0,0 +1,17 @@
+"""
+Efetuando o cálculo da área de um círculo em python - Bee 1002
+
+Name: area_circulo.py
+Descripition: script para realizar o cálculo da área de uma circunferência
+
+Author: Ana Caroline P. e Silva
+Version: 1.0 
+Creation Date: 27/05/2024
+Last Modified: 27/05/2024
+
+"""
+raio = float(input("Digite o valor do raio: "))
+
+area = 3.14159 * raio**(2)
+
+print(f"A={area:.4f}")
