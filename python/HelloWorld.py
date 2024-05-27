daa32d4e0063f0ae5b10343b7382da98d0abccbf
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2024-05-27 18:09:37 -0300

    commit HelloWorld.py
    Primeiro problema resolvido.

---
+++ python/HelloWorld.py
@@ -0,0 +1,13 @@
+"""
+Imprimindo o primeiro Hello World em python
+
+Name: HelloWorld.py
+Descripition: script para imprimir Hello World em python
+
+Author: Ana Caroline P. e Silva
+Version: 1.0 
+Creation Date: 27/05/2024
+Last Modified: 27/05/2024
+
+"""
+print("Hello World!")
