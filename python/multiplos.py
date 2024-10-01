b704bbeb0f9d7ee3b5cb812599ba6ebdf0506b5c
Ana Caroline <155763806+ACPedrosa@users.noreply.github.com>
2024-10-01 12:01:15 -0300

    Update multiplos.py
    Add descrição do código

--- python/multiplos.py
+++ python/multiplos.py
@@ -1,4 +1,14 @@
 '''
+Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos",
+indicando se os valores lidos são múltiplos entre si - Bee 1044
+
+Name: multiplos.py
+Descripition: script para verificar se dois números são múltiplos entre si
+
+Author: Ana Caroline P. e Silva
+Version: 1.0 
+Creation Date: 01/10/2024
+Last Modified: 01/10/2024
 
 '''
 a, b = input().split(" ")
